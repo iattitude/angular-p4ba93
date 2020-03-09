81e9a2bfa55aceef40d63ab3a7bfc1ac124bcdcf
iattitude <mymailbox.de@gmail.com>
2020-03-09 22:17:55 +0530

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { PanelComponent } from './panel/panel.component';
 import { WorkbenchComponent } from './workbench/workbench.component';
+import { WorkbenchCComponent } from './workbench-c/workbench-c.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, PanelComponent, WorkbenchComponent ],
+  declarations: [ AppComponent, PanelComponent, WorkbenchComponent,WorkbenchCComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/workbench-c/workbench-c.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-workbench-c',
+  templateUrl: './workbench-c.component.html',
+  styleUrls: ['./workbench-c.component.css']
+})
+export class WorkbenchCComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
